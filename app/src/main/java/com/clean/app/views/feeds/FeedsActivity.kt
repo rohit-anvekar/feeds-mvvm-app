44252b71306f2ba49418486afc4ee4f940f3982b
rohit-anvekar <anvekar.rohit@gmail.com>
2020-07-14 19:53:01 +0530

    required packages added

---
+++ app/src/main/java/com/clean/app/views/feeds/FeedsActivity.kt
@@ -0,0 +1,13 @@
+package com.clean.app.views.feeds
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.clean.app.R
+
+class FeedsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feeds)
+    }
+}
